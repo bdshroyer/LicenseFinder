4b14f10bf18f090470a0c70c0873142c300be3b1
Sam Coward <idoru42@gmail.com>
2016-11-28 17:39:54 -0500

    Adding Erlang 19.1 to Travis config file.
    Signed-off-by: Brandon Shroyer <bshroyer@pivotal.io>

--- .travis/install_mix.sh
+++ .travis/install_mix.sh
@@ -2,6 +2,11 @@
 set -e
 set -x
 
+
+kerl build 19.1 19.1
+kerl install 19.1 /usr/local/erlang19.1
+. /usr/local/erlang19.1/activate
+
 git clone https://github.com/elixir-lang/elixir.git
 cd elixir
 make install
