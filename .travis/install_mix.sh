7b538e6e6a3f4c633ef1e8ce7a9fb845e208696b
Brandon Shroyer <bshroyer@pivotal.io>
2016-11-29 16:58:06 -0500

    Replace gunzip with unzip in install_mix.sh

--- .travis/install_mix.sh
+++ .travis/install_mix.sh
@@ -7,7 +7,6 @@ pushd ~
   erl -version
 
   wget https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip
-  gunzip Precompiled.zip
-  mv Precompiled elixir
+  unzip Precompiled.zip -d elixir
   export PATH=$PATH:$PWD/elixir/bin
 popd
