22a90f55caf6f616a194f9b782167b9db4f425f7
Sam Coward <idoru42@gmail.com>
2016-11-29 09:39:33 -0500

    Use built-in Elixir support for Travis.
    Signed-off-by: Brandon Shroyer <bshroyer@pivotal.io>

--- .travis/install_mix.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-set -x
-
-
-wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb && dpkg -i erlang-solutions_1.0_all.deb
-apt-get update
-apt-get install esl-erlang
-apt-get install elixir
