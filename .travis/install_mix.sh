f70348c2aa292292031065df4055495792ac279f
Brandon Shroyer <bdshroyer@gmail.com>
2016-12-03 16:10:14 -0500

    Downgrade to Erlang 18.3 and set KERL_BUILD_DOCS env var.

--- .travis/install_mix.sh
+++ .travis/install_mix.sh
@@ -11,8 +11,9 @@ pushd ~
     chmod a+x kerl
   popd
 
-  kerl build 19.1 erlang-19.1
-  kerl install erlang-19.1 erlang
+  export KERL_BUILD_DOCS=yes
+  kerl build 18.3 erlang-18.3
+  kerl install erlang-18.3 erlang
 
   wget https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip
   unzip Precompiled.zip -d elixir
