d31389f495aa1cff86e8c0390d6a00a28edb6184
Brandon Shroyer <bdshroyer@gmail.com>
2016-12-03 15:41:34 -0500

    Install kerl in install_mix script.

--- .travis/install_mix.sh
+++ .travis/install_mix.sh
@@ -4,7 +4,12 @@ set -x
 
 pushd ~
 
-  erl -version
+  mkdir kerl
+
+  pushd kerl
+    curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+    chmod a+x kerl
+  popd
 
   wget https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip
   unzip Precompiled.zip -d elixir
