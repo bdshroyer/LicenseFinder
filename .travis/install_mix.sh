4b7dc8b1813f6fc4fa53029bd1d382bf98ca22b0
Brandon Shroyer <bdshroyer@gmail.com>
2016-12-03 21:13:31 -0500

    Use dpkg -i to to install Erlang.

--- .travis/install_mix.sh
+++ .travis/install_mix.sh
@@ -4,16 +4,20 @@ set -x
 
 pushd ~
 
-  mkdir kerl
+  #mkdir kerl
 
-  pushd kerl
-    curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
-    chmod a+x kerl
-  popd
+  #pushd kerl
+    #curl -O https://raw.githubusercontent.com/kerl/kerl/master/kerl
+    #chmod a+x kerl
+  #popd
 
-  export KERL_BUILD_DOCS=yes
-  kerl build 18.3 erlang-18.3
-  kerl install erlang-18.3 erlang
+  #export KERL_BUILD_DOCS=yes
+  #kerl build 18.3 erlang-18.3
+  #kerl install erlang-18.3 erlang
+
+ mkdir erlang
+ wget https://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+ dpkg -i erlang-solutions_1.0_all.deb --force-not-root --root=erlang
 
   wget https://github.com/elixir-lang/elixir/releases/download/v1.3.4/Precompiled.zip
   unzip Precompiled.zip -d elixir
